56431dd371eca19802dbbc220b9158f87bff8c3f
Antonio Fontaine <afontaine@uc.cl>
2016-06-29 13:34:15 -0400

    a

--- server/models/database.js
+++ server/models/database.js
@@ -12,18 +12,28 @@ if (app.get('env') == "development") {
     query.on('end', function() {
         client.end();
     });
-}
-else {
-  pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    //.query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .query('CREATE TABLE places(id SERIAL PRIMARY KEY, name VARCHAR(80) not null, description VARCHAR(800), is_active BOOLEAN, daily_visits INTEGER, created_at TIMESTAMP, updated_at TIMESTAMP, email VARCHAR(100) not null, email_verified BOOLEAN, status INTEGER, password VARCHAR(300))')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
+} else {
+    pg.defaults.ssl = true;
+    console.log(process.env.DATABASE_URL);
+    var client = new pg.Client(process.env.DATABASE_URL);
+    console.log("cliente");
+    client.connect();
+    console.log("conectado");
+    var query = client.query('CREATE TABLE places(id SERIAL PRIMARY KEY, name VARCHAR(80) not null, description VARCHAR(800), is_active BOOLEAN, daily_visits INTEGER, created_at TIMESTAMP, updated_at TIMESTAMP, email VARCHAR(100) not null, email_verified BOOLEAN, status INTEGER, password VARCHAR(300))');
+    query.on('end', function() {
+        console.log("fin");
+        client.end();
     });
-});
+
+    // pg.connect(process.env.DATABASE_URL, function(err, client) {
+    //     if (err) throw err;
+    //     console.log('Connected to postgres! Getting schemas...');
+    //
+    //     client
+    //     //.query('SELECT table_schema,table_name FROM information_schema.tables;')
+    //         .query('CREATE TABLE places(id SERIAL PRIMARY KEY, name VARCHAR(80) not null, description VARCHAR(800), is_active BOOLEAN, daily_visits INTEGER, created_at TIMESTAMP, updated_at TIMESTAMP, email VARCHAR(100) not null, email_verified BOOLEAN, status INTEGER, password VARCHAR(300))')
+    //         .on('row', function(row) {
+    //             console.log(JSON.stringify(row));
+    //         });
+    //});
 }

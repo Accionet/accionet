d288da83e34a37892cabdbe6f040600506bc6caa
Antonio Fontaine <afontaine@uc.cl>
2016-09-04 17:08:53 -0300

    Backend to get all the responses of a particular survey

--- server/Tests/test1.js
+++ server/Tests/test1.js
@@ -2,17 +2,12 @@ var Places = require('../models/places.js');
 var Surveys = require('../models/surveys.js');
 var Questions = require('../models/questions.js');
 var Response = require('../models/response.js');
+var Answer = require('../models/answer.js');
 
 
-var response = {
-    survey_id: 2,
-    answers: [{
-        question_id: 3,
-        answer_option_id: 4
-    }]
-};
 
-Response.save(response, function(err, result) {
+
+Answer.findOfSurvey(2, function(err, result) {
     if (err) {
         console.error(err);
         return err;

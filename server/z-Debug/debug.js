cb8e8875fbce900adc6b1f5a3d3e06ff1e7ded7b
Antonio Fontaine <afontaine@uc.cl>
2016-11-03 16:26:56 -0300

    hotfix

--- server/z-Debug/debug.js
+++ server/z-Debug/debug.js
@@ -6,7 +6,5 @@ const options = require('../newModels/options');
 
 const utils = require('../services/utils');
 
-
-options.all().then((a) => {
-  console.log(a);
-});
+console.log(options.table_name);
+console.log(options.table());

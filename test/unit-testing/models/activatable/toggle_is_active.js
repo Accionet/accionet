f4ab3357558a295e43a81de17067b8d73c2f68ad
Antonio Fontaine <afontaine@uc.cl>
2016-11-19 17:59:39 -0300

    test to check activatable's toggleIsActive

---
+++ test/unit-testing/models/activatable/toggle_is_active.js
@@ -0,0 +1,53 @@
+'use strict';
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+const dateChai = require('chai-datetime');
+
+const Place = require('../../../../server/newModels/places');
+
+
+// eslint-disable-next-line no-unused-vars
+const assert = chai.assert;
+// eslint-disable-next-line no-unused-vars
+const should = chai.should();
+
+chai.use(dateChai);
+
+
+// eslint-disable-next-line no-undef
+describe('Activatable: toggleIsActive', () => {
+  it('toggle from true to false', (done) => { // eslint-disable-line no-undef
+    const place = {
+      name: 'place',
+      is_active: true,
+    };
+    return Place.save(place).then((place) => {
+      Place.toggleIsActive(place.id).then((place) => {
+        assert.equal(place.is_active, false);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    }).catch((err) => {
+      done(err);
+    });
+  });
+
+  it('toggle from false to true', (done) => { // eslint-disable-line no-undef
+    const place = {
+      name: 'place',
+      is_active: false,
+    };
+    return Place.save(place).then((place) => {
+      Place.toggleIsActive(place.id).then((place) => {
+        assert.equal(place.is_active, true);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    }).catch((err) => {
+      done(err);
+    });
+  });
+});

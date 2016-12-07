0be10637c90aff12b45c6b503c8414ed875b73ae
Antonio Fontaine <afontaine@uc.cl>
2016-12-07 19:08:12 -0300

    Adapt hotspot test

--- devHotspot/js/counter.js
+++ devHotspot/js/counter.js
@@ -1,46 +1,48 @@
+/* eslint-disable*/
 $(function () {
-    const url = 'http://localhost:3000/';
+  const url = 'http://localhost:3000/';
 
-    const macaddress = $('#macAddress').val();
-    $.post(url + 'visits/new', {
-        macaddress,
-        place_id: 1,
-    });
-
-
-    const destination = '/makeLogin.html';
+  const macaddress = $('#macAddress').val();
+  $.post(url + 'visits/new', {
+    macaddress,
+    place_id: 18,
+  });
 
 
-    const $sendSurvey = $('#sendSurvey');
+  const destination = '/makeLogin.html';
 
 
-    $sendSurvey.on('click', function (event) {
-        event.preventDefault();
-        const json = {
-            survey_id: 3,
-            answers: [{
-                question_id: 6,
-                answer_option_id: 34,
-            }],
-            macaddress,
-        };
-        const json_send = JSON.stringify(json);
-        console.log('mandar encuesta');
-        $.ajax({
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                string_json: json_send,
-            },
-            url: (url + 'survey/3/response'),
-            success() {
-                console.log('success');
+  const $sendSurvey = $('#sendSurvey');
+
+  const survey_id = 19;
+
+  $sendSurvey.on('click', function (event) {
+    event.preventDefault();
+    const json = {
+      survey_id,
+      answers: [{
+        question_id: 38,
+        answer_option_id: $('input[name=user_age]:checked').val(),
+      }],
+      macaddress,
+    };
+    const json_send = JSON.stringify(json);
+    console.log('mandar encuesta');
+    $.ajax({
+      type: 'POST',
+      dataType: 'json',
+      data: {
+        string_json: json_send,
+      },
+      url: (url + `survey/${survey_id}/response`),
+      success() {
+        console.log('success');
                 // window.location = destination;
-            },
-            error(err) {
-                console.log('error');
-                console.log(err);
-            },
-        });
+      },
+      error(err) {
+        console.log('error');
+        console.log(err);
+      },
     });
+  });
 });

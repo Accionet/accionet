1813e0477e94030bfc3f5e5c2e1f3aaa54659483
Antonio Fontaine <afontaine@uc.cl>
2016-12-09 16:49:59 -0300

    Visits number are shown with ks and m in dashboard

--- client/public/javascripts/angular/controllers/dashboard-controller.js
+++ client/public/javascripts/angular/controllers/dashboard-controller.js
@@ -27,10 +27,38 @@ controllers
     $scope[entry].loading = true;
     $http.get(`/${entry}/count`)
       .success(function success(data) {
-        console.log(data);
-        $scope[entry].count = data.amount;
+        $scope[entry].count = toSimpleString(data.amount);
         $scope[entry].loading = false;
       })
       .error(function error() {});
   };
+
+  function roundDecimal(num) {
+    return Math.round(num * 10) / 10;
+  }
+
+  function toSimpleString(amount) {
+    const one_million = 1000000;
+    const one_thousand = 1000;
+    const ten_thousand = 10000;
+    const fifty_million = 5000000;
+
+    amount = parseInt(amount, 10);
+    if (amount >= fifty_million) {
+      return '>50M';
+    }
+    if (amount >= one_million) {
+      amount = amount / one_million;
+      return roundDecimal(amount) + 'M';
+    }
+    if (amount >= ten_thousand) {
+      amount = amount / one_thousand;
+      return Math.round(amount) + 'K';
+    }
+    if (amount >= one_thousand) {
+      amount = amount / one_thousand;
+      return roundDecimal(amount) + 'K';
+    }
+    return amount;
+  }
 });

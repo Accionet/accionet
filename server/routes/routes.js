1cf5e326c4ea641a01c568ad60252521ed94fc4d
Antonio Fontaine <afontaine@uc.cl>
2017-01-30 12:01:11 -0300

    Access restricted to user logged in

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -40,7 +40,7 @@ module.exports = function router(app, passport) {
 
 
   /* GET home page. */
-  app.get('/dashboard', (req, res, next) => {
+  app.get('/dashboard', isLoggedIn, (req, res, next) => {
     dashboardController.show(req, res, next);
     // res.sendFile(path.join(__dirname, '../','../', 'client','views', 'index.html'));
   });
@@ -62,44 +62,44 @@ module.exports = function router(app, passport) {
   // //////////////////////////////////////////
 
 
-  app.get('/places', (req, res, next) => {
+  app.get('/places', isLoggedIn, (req, res, next) => {
     placeController.index(req, res, next);
   });
 
-  app.get('/places/disabled', (req, res, next) => {
+  app.get('/places/disabled', isAdmin, (req, res, next) => {
     placeController.disabled(req, res, next);
   });
 
-  app.get('/places/count', (req, res, next) => {
+  app.get('/places/count', isLoggedIn, (req, res, next) => {
     placeController.count(req, res, next);
   });
 
-  app.get('/places/:id/edit', (req, res, next) => {
+  app.get('/places/:id/edit', hasAccessToWrite, (req, res, next) => {
     placeController.edit(req, res, next);
   });
 
 
-  app.get('/places/new', (req, res, next) => {
+  app.get('/places/new', isAdmin, (req, res, next) => {
     placeController.new(req, res, next);
   });
 
-  app.get('/places/:id', (req, res, next) => {
+  app.get('/places/:id', hasAccessToRead, (req, res, next) => {
     placeController.show(req, res, next);
   });
 
-  app.post('/places/new', (req, res, next) => {
+  app.post('/places/new', isAdmin, (req, res, next) => {
     placeController.create(req, res, next);
   });
 
-  app.put('/places/:id/edit', (req, res, next) => {
+  app.put('/places/:id/edit', hasAccessToWrite, (req, res, next) => {
     placeController.update(req, res, next);
   });
 
-  app.put('/places/:id/toggleIsActive', (req, res, next) => {
+  app.put('/places/:id/toggleIsActive', hasAccessToWrite, (req, res, next) => {
     placeController.toggleIsActive(req, res, next);
   });
 
-  app.get('/places/:id/metrics', (req, res, next) => {
+  app.get('/places/:id/metrics', hasAccessToRead, (req, res, next) => {
     placeController.metrics(req, res, next);
   });
 
@@ -109,40 +109,40 @@ module.exports = function router(app, passport) {
   // //////////////////////////////////////////
 
 
-  app.get('/surveys', (req, res, next) => {
+  app.get('/surveys', isLoggedIn, (req, res, next) => {
     surveyController.index(req, res, next);
   });
 
-  app.get('/surveys/disabled', (req, res, next) => {
+  app.get('/surveys/disabled', isAdmin, (req, res, next) => {
     surveyController.disabled(req, res, next);
   });
 
 
-  app.get('/surveys/new', (req, res, next) => {
+  app.get('/surveys/new', isAdmin, (req, res, next) => {
     surveyController.new(req, res, next);
   });
 
-  app.post('/surveys/new', (req, res, next) => {
+  app.post('/surveys/new', isAdmin, (req, res, next) => {
     surveyController.create(req, res, next);
   });
 
-  app.get('/surveys/count', (req, res, next) => {
+  app.get('/surveys/count', isLoggedIn, (req, res, next) => {
     surveyController.count(req, res, next);
   });
 
-  app.get('/surveys/:id', (req, res, next) => {
+  app.get('/surveys/:id', hasAccessToRead, (req, res, next) => {
     surveyController.show(req, res, next);
   });
 
-  app.get('/surveys/:id/edit', (req, res, next) => {
+  app.get('/surveys/:id/edit', hasAccessToWrite, (req, res, next) => {
     surveyController.edit(req, res, next);
   });
 
-  app.get('/surveys/:id/metrics', (req, res, next) => {
+  app.get('/surveys/:id/metrics', hasAccessToRead, (req, res, next) => {
     surveyController.metrics(req, res, next);
   });
 
-  app.get('/surveys/:id/excel', (req, res, next) => {
+  app.get('/surveys/:id/excel', hasAccessToRead, (req, res, next) => {
     surveyController.generateExcel(req, res, next);
   });
 
@@ -150,39 +150,39 @@ module.exports = function router(app, passport) {
     surveyController.responseSurvey(req, res, next);
   });
 
-  app.put('/surveys/:id/toggleIsActive', (req, res, next) => {
+  app.put('/surveys/:id/toggleIsActive', hasAccessToWrite, (req, res, next) => {
     surveyController.toggleIsActive(req, res, next);
   });
 
-  app.put('/surveys/:id/update', (req, res, next) => {
+  app.put('/surveys/:id/update', hasAccessToWrite, (req, res, next) => {
     surveyController.update(req, res, next);
   });
 
-  app.delete('/surveys/:id/delete', (req, res, next) => {
+  app.delete('/surveys/:id/delete', hasAccessToWrite, (req, res, next) => {
     surveyController.delete(req, res, next);
   });
 
 
-  app.get('/responses/count', (req, res, next) => {
+  app.get('/responses/count', isLoggedIn, (req, res, next) => {
     responseController.count(req, res, next);
   });
 
 
   // API
 
-  app.get('/api/v1/surveys/:id/metrics/responses/byhour', (req, res, next) => {
+  app.get('/api/v1/surveys/:id/metrics/responses/byhour', hasAccessToRead, (req, res, next) => {
     surveyController.metricsByHour(req, res, next);
   });
 
-  app.get('/api/v1/surveys/:id/metrics/responses/byday', (req, res, next) => {
+  app.get('/api/v1/surveys/:id/metrics/responses/byday', hasAccessToRead, (req, res, next) => {
     surveyController.metricsByDay(req, res, next);
   });
 
-  app.get('/api/v1/surveys/:id/metrics/responses/count', (req, res, next) => {
+  app.get('/api/v1/surveys/:id/metrics/responses/count', hasAccessToRead, (req, res, next) => {
     surveyController.countResponses(req, res, next);
   });
 
-  app.get('/api/v1/surveys/:id/metrics/enduser/count', (req, res, next) => {
+  app.get('/api/v1/surveys/:id/metrics/enduser/count', hasAccessToRead, (req, res, next) => {
     surveyController.countEndUser(req, res, next);
   });
 
@@ -195,31 +195,31 @@ module.exports = function router(app, passport) {
     visitController.create(req, res, next);
   });
 
-  app.get('/visits/count', (req, res, next) => {
+  app.get('/visits/count', isLoggedIn, (req, res, next) => {
     visitController.count(req, res, next);
   });
 
-  app.get('/api/v1/places/:id/metrics/visits/daily', (req, res, next) => {
+  app.get('/api/v1/places/:id/metrics/visits/daily', hasAccessToRead, (req, res, next) => {
     visitController.dailyTable(req, res, next);
   });
 
-  app.get('/api/v1/places/:id/metrics/visits/hourly', (req, res, next) => {
+  app.get('/api/v1/places/:id/metrics/visits/hourly', hasAccessToRead, (req, res, next) => {
     visitController.hourlyTable(req, res, next);
   });
 
-  app.get('/api/v1/places/:id/metrics/visits/dayandhour', (req, res, next) => {
+  app.get('/api/v1/places/:id/metrics/visits/dayandhour', hasAccessToRead, (req, res, next) => {
     visitController.dayAndHourTable(req, res, next);
   });
 
-  app.get('/api/v1/places/:id/metrics/visits/count', (req, res, next) => {
+  app.get('/api/v1/places/:id/metrics/visits/count', hasAccessToRead, (req, res, next) => {
     visitController.countOfPlace(req, res, next);
   });
 
-  app.get('/api/v1/surveys/:id/metrics/visits/count', (req, res, next) => {
+  app.get('/api/v1/surveys/:id/metrics/visits/count', hasAccessToRead, (req, res, next) => {
     visitController.countOfSurvey(req, res, next);
   });
 
-  app.get('/api/v1/places/:id/metrics/endusers/count', (req, res, next) => {
+  app.get('/api/v1/places/:id/metrics/endusers/count', hasAccessToRead, (req, res, next) => {
     visitController.countEndUsersOfPlace(req, res, next);
   });
 
@@ -228,7 +228,7 @@ module.exports = function router(app, passport) {
   // ///////// USERS  ////////////////////////
   // ////////////////////////////////////////
 
-  app.get('/users/count', (req, res, next) => {
+  app.get('/users/count', isLoggedIn, (req, res, next) => {
     userController.count(req, res, next);
   });
 };
@@ -238,13 +238,43 @@ module.exports = function router(app, passport) {
 // //////////////////////////////////////////
 //
 //
-// // route middleware to make sure a user is logged in
-// function isLoggedIn(req, res, next) {
-//
-//     // if user is authenticated in the session, carry on
-//     if (req.isAuthenticated())
-//         return next();
-//
-//     // if they aren't redirect them to the home page
-//     res.redirect('/');
-// }
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+    // if they aren't redirect them to the home page
+  res.redirect('/');
+}
+
+function hasAccessToWrite(req, res, next) {
+    // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+    // if they aren't redirect them to the home page
+  res.redirect('/');
+}
+
+function hasAccessToRead(req, res, next) {
+    // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+    // if they aren't redirect them to the home page
+  res.redirect('/');
+}
+
+function isAdmin(req, res, next) {
+    // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+    // if they aren't redirect them to the home page
+  res.redirect('/');
+}

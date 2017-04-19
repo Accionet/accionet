8f36d1f2f753f711672583413eda3189c8ff2824
Antonio Fontaine <afontaine@uc.cl>
2017-04-19 18:20:55 -0300

    Able to get visits by day with timezone offset

--- server/models/decorators/DayMetrics.js
+++ server/models/decorators/DayMetrics.js
@@ -1,18 +1,27 @@
 // Amount By Day
 const knex = require('../../db/knex');
 const TimeAdapter = require('../../adapters/timeAdapter');
+const dbUtils = require('../../services/dbUtils');
 
-function getGroupedByDay(searchParams) {
-  return this.table()
-    .select(knex.raw('EXTRACT(year from created_at) as year, EXTRACT(DOY from created_at) as doy, count(*) as amount'))
-    .where(searchParams)
-    .groupByRaw('year, doy')
-    .orderByRaw('year, doy');
+
+function getGroupedByDay(searchParams, minUTCOffset) {
+  return new Promise((resolve, reject) => {
+    dbUtils.getMinutesInterval(minUTCOffset).then((minOffset) => {
+      resolve(this.table()
+        .select(knex.raw(`EXTRACT(year from (created_at + INTERVAL '${minOffset} minutes')) as year, EXTRACT(DOY from (created_at + INTERVAL '${minOffset} minutes')) as doy, count(*) as amount`))
+        .where(searchParams)
+        .groupByRaw('year, doy')
+        .orderByRaw('year, doy')
+      );
+    }).catch((err) => {
+      reject(err);
+    });
+  });
 }
 
-function byDay(searchParams) {
+function byDay(searchParams, minUTCOffset) {
   return new Promise((resolve, reject) => {
-    this.getGroupedByDay(searchParams).then((entries) => {
+    this.getGroupedByDay(searchParams, minUTCOffset).then((entries) => {
       const parsedEntries = TimeAdapter.mapForDailyGraph(entries);
       resolve(parsedEntries);
     }).catch((err) => {
--- test/unit-testing/decorators/visitMetric/byDay.js
+++ test/unit-testing/decorators/visitMetric/byDay.js
@@ -22,94 +22,51 @@ const should = chai.should();
 chai.use(dateChai);
 
 
-// // eslint-disable-next-line no-undef
-// describe('Visits: Malicious byDay', () => {
-//   // eslint-disable-next-line no-undef
-//   it('Pass as parameter something that is a json, without id attribute', (done) => {
-//     const place = {
-//       key: 'value',
-//     };
-//     return Visits.byDay(place).then(() => {
-//       done('It should not get to here');
-//     }).catch(() => {
-//       done();
-//     });
-//   });
-//
-//   // eslint-disable-next-line no-undef
-//   it('Pass as parameter something that is a json, with id. but not a valid id', (done) => {
-//     const place = {
-//       place_id: 'value',
-//     };
-//     return Visits.byDay(place).then(() => {
-//       done('It should not get to here');
-//     }).catch(() => {
-//       done();
-//     });
-//   });
-//
-//   // eslint-disable-next-line no-undef
-//   it('Pass as parameter something that is a json, with id. But not a negative id', (done) => {
-//     const place = {
-//       place_id: -2,
-//     };
-//     return Visits.byDay(place).then((response) => {
-//       response.should.be.a('array');
-//       assert.equal(response.length, 0);
-//       done();
-//     }).catch((error) => {
-//       done(error);
-//     });
-//   });
-// });
-//
-// // eslint-disable-next-line no-undef
-// describe('Visits: byDay', () => {
-//   // eslint-disable-next-line no-undef
-//
-//
-//   // eslint-disable-next-line no-undef
-//   it('Check valid response', (done) => {
-//     return Place.all().then((places) => {
-//       const randomIndex = utils.randomInteger(0, places.length - 1);
-//       const place = places[randomIndex];
-//       Visits.byDay({
-//         place_id: place.id,
-//       }).then((table) => {
-//         table.should.be.a('array');
-//         const randomIndex = utils.randomInteger(0, table.length - 1);
-//         const entry = table[randomIndex];
-//         entry.should.be.a('array');
-//         try {
-//           const date = new Date(entry[0]); //eslint-disable-line
-//         } catch (Exception) {
-//           done('Not a valid date');
-//         }
-//         const amount = parseInt(entry[1], 10);
-//         amount.should.be.a('number');
-//
-//         done();
-//       }).catch((error) => {
-//         done(error);
-//       });
-//     }).catch((error) => {
-//       done(error);
-//     });
-//   });
-// });
+// eslint-disable-next-line no-undef
+describe('Visits: Malicious byDay', () => {
+  // eslint-disable-next-line no-undef
+  it('Pass as parameter something that is a json, without id attribute', (done) => {
+    const place = {
+      key: 'value',
+    };
+    return Visits.byDay(place).then(() => {
+      done('It should not get to here');
+    }).catch(() => {
+      done();
+    });
+  });
+
+  // eslint-disable-next-line no-undef
+  it('Pass as parameter something that is a json, with id. but not a valid id', (done) => {
+    const place = {
+      place_id: 'value',
+    };
+    return Visits.byDay(place).then(() => {
+      done('It should not get to here');
+    }).catch(() => {
+      done();
+    });
+  });
+
+  // eslint-disable-next-line no-undef
+  it('Pass as parameter something that is a json, with id. But not a negative id', (done) => {
+    const place = {
+      place_id: -2,
+    };
+    return Visits.byDay(place).then((response) => {
+      response.should.be.a('array');
+      assert.equal(response.length, 0);
+      done();
+    }).catch((error) => {
+      done(error);
+    });
+  });
+});
 
 // eslint-disable-next-line no-undef
-describe('Visits: byDay with timezone changed', () => {
+describe('Visits: byDay', () => {
   // eslint-disable-next-line no-undef
-  // eslint-disable-next-line no-undef
-  before((done) => {
-    return Promise.all([knex.raw('SELECT EXTRACT(TIMEZONE from now()) as sec_offset')])
-      .then(() => {
-        done();
-      }).catch((err) => {
-        done(err);
-      });
-  });
+
 
   // eslint-disable-next-line no-undef
   it('Check valid response', (done) => {
@@ -118,15 +75,59 @@ describe('Visits: byDay with timezone changed', () => {
       const place = places[randomIndex];
       Visits.byDay({
         place_id: place.id,
+      }).then((table) => {
+        table.should.be.a('array');
+        const randomIndex = utils.randomInteger(0, table.length - 1);
+        const entry = table[randomIndex];
+        entry.should.be.a('array');
+        try {
+          const date = new Date(entry[0]); //eslint-disable-line
+        } catch (Exception) {
+          done('Not a valid date');
+        }
+        const amount = parseInt(entry[1], 10);
+        amount.should.be.a('number');
+
+        done();
+      }).catch((error) => {
+        done(error);
+      });
+    }).catch((error) => {
+      done(error);
+    });
+  });
+});
+
+// eslint-disable-next-line no-undef
+describe('Visits: byDay with timezone changed', () => {
+  let bd_offset = 0;
+  // eslint-disable-next-line no-undef
+  before((done) => {
+    return Promise.all([knex.raw('SELECT EXTRACT(TIMEZONE from now()) as sec_offset')])
+      .then((results) => {
+        bd_offset = results[0].rows[0].sec_offset / 60;
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+  });
+
+  // eslint-disable-next-line no-undef
+  it('Check one day delay', (done) => {
+    return Place.all().then((places) => {
+      const randomIndex = utils.randomInteger(0, places.length - 1);
+      const place = places[randomIndex];
+      Visits.byDay({
+        place_id: place.id,
       }).then((noOffset) => {
         Visits.byDay({
           place_id: place.id,
-        }, 24 * 60).then((withOffset) => {
+        }, (24 * 60) + bd_offset).then((withOffset) => {
           for (let i = 0; i < noOffset.length; i++) {
             // quantities should not change
             assert.equal(noOffset[i][1], withOffset[i][1]);
-            // time difference must be 24 hours
-            assert.equal(new Date(noOffset[i][0]).getTime() + (24 * 60 * 60 * 1000), new Date(withOffset[i][0]).getTime());
+            // time difference must be 1 day
+            assert.equal((new Date(withOffset[i][0]).getTime() - new Date(noOffset[i][0]).getTime()) / (24 * 60 * 60 * 1000), 1);
           }
           done();
         }).catch((error) => {

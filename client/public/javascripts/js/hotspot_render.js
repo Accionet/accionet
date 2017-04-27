cef322b0697eb267646ec8dfca2533fd8b42469d
Antonio Fontaine <afontaine@uc.cl>
2017-04-27 10:51:42 -0300

    Upload image and change in hotspot preview

--- client/public/javascripts/js/hotspot_render.js
+++ client/public/javascripts/js/hotspot_render.js
@@ -5,6 +5,8 @@ $(function () {
   var converter = new showdown.Converter();
   var pad = $('#pad');
   var text = $('#markdown');
+  var image = $('.hotspot img');
+  var fileupload = $('#image-upload');
 
   var convertTextAreaToMarkdown = function () {
     var markdownText = pad.val();
@@ -15,5 +17,29 @@ $(function () {
   pad.bind('input propertychange', function() {
     convertTextAreaToMarkdown();
   })
+
+
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+        console.log('ENTRO');
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              image.attr('src', e.target.result);
+          }
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
+
+  $("#imgInp").change(function(){
+      readURL(this);
+  });
+
+  fileupload.on('change', function (event) {
+    readURL(this)
+    // console.log(fileupload.val());
+    // image.attr('src', fileupload.val());
+  });
 
 });

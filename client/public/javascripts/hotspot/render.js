0bbba869ba2024dec8582da475dec6cef29bbd64
Antonio Fontaine <afontaine@uc.cl>
2017-04-27 15:01:12 -0300

    Change image of iframe

--- client/public/javascripts/hotspot/render.js
+++ client/public/javascripts/hotspot/render.js
@@ -4,7 +4,7 @@ $(function() {
 
   var converter = new showdown.Converter();
   var text = $('#markdown');
-  var image = $('.hotspot img');
+  var $imageTemplate = $('#imageTemplate');
   var fileupload = $('#image-upload');
   var changeHotspotSize = $('.change_size');
   var rotateHotspot = $('.rotate')
@@ -14,11 +14,10 @@ $(function() {
 
 
 
+
   var convertToHtml = function convertToHtml() {
     $('#hotspot').remove();
-    console.log('converting ');
-    var text = $('#imageTemplate').val();
-    console.log(text);
+    var text = $imageTemplate.val();
     var iframe = $('<iframe frameborder="0" name="hotspot" id="hotspot"> </iframe>');
     iframe.appendTo('#hotspotContainer');
     var iframewindow = iframe[0].contentWindow ? iframe[0].contentWindow : iframe[0].contentDocument.defaultView;
@@ -32,13 +31,25 @@ $(function() {
 
 
 
+  function changeImage(path) {
+    var html = $imageTemplate.val();
+    var startPoint = $imageTemplate.data('imageSrc');
+    var startQuote = html.indexOf('"', startPoint);
+    var endQuote = html.indexOf('"', startQuote + 1);
+    var endHtml = html.substring(0, startQuote + 1) + path + html.substring(endQuote, html.length);
+    $imageTemplate.text(endHtml);
+    convertToHtml();
+  }
+
+
   function readURL(input) {
     if (input.files && input.files[0]) {
-      console.log('ENTRO');
+      console.log('reaad IRL');
       var reader = new FileReader();
 
       reader.onload = function(e) {
-        image.attr('src', e.target.result);
+        changeImage(e.target.result);
+        // console.log(e.target.result);
       }
 
       reader.readAsDataURL(input.files[0]);
@@ -51,8 +62,6 @@ $(function() {
 
   fileupload.on('change', function(event) {
     readURL(this)
-      // console.log(fileupload.val());
-      // image.attr('src', fileupload.val());
   });
 
 });

42f5b93846ba45065f2147b615f64ba4c925f0cb
Antonio Fontaine <afontaine@uc.cl>
2017-04-27 12:12:58 -0300

    Change image size

--- client/public/javascripts/js/hotspot_render.js
+++ client/public/javascripts/js/hotspot_render.js
@@ -1,14 +1,17 @@
 /* public/script.js */
 /* eslint-disable */
-$(function () {
+$(function() {
 
   var converter = new showdown.Converter();
   var pad = $('#pad');
   var text = $('#markdown');
   var image = $('.hotspot img');
   var fileupload = $('#image-upload');
+  var changeHotspotSize = $('.change_size');
+  var rotateHotspot = $('.rotate')
+  var current_view = 'mobile';
 
-  var convertTextAreaToMarkdown = function () {
+  var convertTextAreaToMarkdown = function() {
     var markdownText = pad.val();
     var html = converter.makeHtml(markdownText);
     text.html(html);
@@ -19,27 +22,57 @@ $(function () {
   })
 
 
-  function readURL(input) {
-      if (input.files && input.files[0]) {
-        console.log('ENTRO');
-          var reader = new FileReader();
+  rotateHotspot.on('click', function (event) {
 
-          reader.onload = function (e) {
-              image.attr('src', e.target.result);
-          }
+  })
 
-          reader.readAsDataURL(input.files[0]);
-      }
+function disableRotator() {
+  if(current_view === 'computer'){
+    rotateHotspot.addClass('text-muted');
+  } else {
+    rotateHotspot.removeClass('text-muted');
+
   }
+}
 
-  $("#imgInp").change(function(){
-      readURL(this);
+
+
+  changeHotspotSize.on('click', function(event) {
+    var width = $(this).data('width') + "px";
+    var height = $(this).data('height') + "px";
+    current_view = $(this).data('name');
+    disableRotator();
+
+    $('.screen').width(width);
+    $('.screen').height(height);
+    $('.screen').css({
+        "max-height": height,
+        "max-width": width
+ });
+
   });
 
-  fileupload.on('change', function (event) {
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      console.log('ENTRO');
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        image.attr('src', e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("#imgInp").change(function() {
+    readURL(this);
+  });
+
+  fileupload.on('change', function(event) {
     readURL(this)
-    // console.log(fileupload.val());
-    // image.attr('src', fileupload.val());
+      // console.log(fileupload.val());
+      // image.attr('src', fileupload.val());
   });
 
 });

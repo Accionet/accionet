386529f03baedf305743a2d05a580316220623a7
Antonio Fontaine <afontaine@uc.cl>
2017-07-10 20:47:41 -0400

    Upload multiple files

--- client/public/javascripts/angular/controllers/hotspots/hotspot_ng_controller.js
+++ client/public/javascripts/angular/controllers/hotspots/hotspot_ng_controller.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
-controllers.controller('hotspotController', function($scope, $http, $window, Utils, placeService) {
+controllers.controller('hotspotController', function($scope, $http, $window, $location, Utils, placeService) {
 
   $scope.i = 1; // current_step
   $scope.selectedHotspot;
   $scope.places;
   $scope.loadingPlaces = true;
   $scope.loadPlacesFailed = false;
+  $scope.completed = 0;
+  $scope.TEMPLATE = 'LANDING-PAGE';
+  $scope.FILE_KEYS = ['IMAGE-PATH', 'BACKGROUND-IMAGE']; //FIXME
 
 
   // Get all places
@@ -160,40 +163,57 @@ controllers.controller('hotspotController', function($scope, $http, $window, Uti
   }
 
   $scope.saveHotspot = function() {
-    $(function() {
-      var f = document.getElementById('image-file').files[0];
-    });
+    $scope.completed = 0;
+    canceled = false;
     var values = parseValuesToSend();
+    console.log(values);
     $scope.selectedHotspot.template = "LANDING-PAGE";
     $http.post('/hotspots/save/', {
-        template_id: "LANDING-PAGE",
+        template_id: $scope.TEMPLATE,
         hotspotInfo: $scope.selectedHotspot,
         template: $scope.current_hotspot.template,
         values: values,
       })
       .success(function(data) {
-        var files = document.getElementById('image-file').files;
-        var file = files[0];
-        if (file == null) {
-          return alert('No file selected.');
-        }
-        var path = data.imageFolder + 'IMAGE-PATH';
-        getSignedRequest(file, path);
+        console.log('se guardo en heroku');
+        changeCompleted(10);
+        var path = data.imageFolder;
+        uploadFiles(path);
       })
-      .error(function(error) {
-        console.log(error);
-      });
-
+      .error(function(error) {});
   }
 
-  function getSignedRequest(file, path) {
+
+var filesToSend = 0;
+var uploadedFiles = 0;
+
+  function uploadFiles(path) {
+    filesToSend = 0;
+    uploadedFiles = 0;
+    if (!canceled) {
+      progressPerFile = 80 / $scope.FILE_KEYS.length;
+      for (var i = 0; i < $scope.FILE_KEYS.length; i++) {
+        var file = document.getElementById($scope.FILE_KEYS[i]).files[0];
+        console.log(file);
+        if (file !== null && file !== undefined) {
+          filesToSend +=1;
+          var filePath = path  + $scope.FILE_KEYS[i];
+          getSignedRequest(file, filePath, progressPerFile);
+        }
+      }
+    }
+  }
+
+  function getSignedRequest(file, filePath, increment) {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', `/sign-s3?file-name=${path}&file-type=${file.type}`);
+    xhr.open('GET', `/sign-s3?file-name=${filePath}&file-type=${file.type}`);
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
+          console.log(file);
+          changeCompleted(increment/2);
           const response = JSON.parse(xhr.responseText);
-          uploadFile(file, response.signedRequest, response.url);
+          uploadFile(file, response.signedRequest, response.url, increment/2);
         } else {
           alert('Could not get signed URL.');
         }
@@ -202,22 +222,37 @@ controllers.controller('hotspotController', function($scope, $http, $window, Uti
     xhr.send();
   }
 
-  function uploadFile(file, signedRequest, url) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('PUT', signedRequest);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          // notify all good
-        } else {
-          alert('Could not upload file.');
+  function uploadFile(file, signedRequest, url, progress) {
+    if (!canceled) {
+      const xhr = new XMLHttpRequest();
+      xhr.open('PUT', signedRequest);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            uploadedFiles += 1;
+            changeCompleted(progress, true);
+          } else {
+            alert('Could not upload file.');
+          }
         }
-      }
-    };
-    xhr.send(file);
+      };
+      xhr.send(file);
+    }
   }
 
+  function changeCompleted(increment, endable) {
+    $scope.completed += increment;
+    $scope.$apply();
+    console.log($scope.completed);
+    if(uploadedFiles == filesToSend && endable) {
+      $window.location.href = '/hotspots/';
 
+    }
+  }
+
+  $scope.cancelUpload = function() {
+    canceled = true;
+  }
 
   $scope.getTemplate("image", true);
 });
--- client/public/javascripts/angular/services/utils.js
+++ client/public/javascripts/angular/services/utils.js
@@ -3,21 +3,27 @@ services
 
   .service('Utils', function() {
 
-    this.parseJson = function (string) {
-      string = jsonEscape(string);
-      return JSON.parse(string);
+  this.parseJson = function(string) {
+    string = jsonEscape(string);
+    return JSON.parse(string);
 
-    }
+  }
 
-    function jsonEscape(str)  {
-        var s = str.split('"');
-        var res = s[0]
-        for (var i = 1; i < s.length; i ++){
-          if(!Number.isInteger(i/2)){
-            s[i] = s[i].replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
-          }
-          res += '"' + s[i]
-        }
-        return res
+  function jsonEscape(str) {
+    var s = str.split('"');
+    var res = s[0]
+    for (var i = 1; i < s.length; i++) {
+      if (!Number.isInteger(i / 2)) {
+        s[i] = s[i].replace(/\n/g, "\\\\n").replace(/\r/g, "\\\\r").replace(/\t/g, "\\\\t");
+      }
+      res += '"' + s[i]
     }
+    return res
+  }
+
+  this.pushNotNull = function(array, value) {
+    if (value !== null) {
+      array.push(value);
+    }
+  }
 });
